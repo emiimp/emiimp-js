4c6737ceb29bc2fd60dbf72a0d6eb6d92360c47e
emiimp <emiliadasmode@gmail.com>
2022-06-30 18:00:37 -0300

    first commit. primer desafio complementario

---
+++ index.js
@@ -0,0 +1,15 @@
+let nombre = prompt("Ingrese su nombre");
+
+for(let i = 0; i <=3; i++){
+    {
+    alert(`¡4 veces hola, ${nombre}!`);
+}
+}
+
+let numero = parseInt(prompt(`${nombre}, por favor ingresá un número para multiplicar tres veces x2`));
+
+for (let index = 2; index <=4; index++){
+    let result = numero * index;
+    let mensaje = `${numero} x ${index} =${result}`;
+    alert(mensaje); 
+}alert("Muchas gracias")
