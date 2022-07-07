4150934c81abee56aaeca62a45fdf6191faf93d0
emiimp <emiliadasmode@gmail.com>
2022-07-07 18:15:42 -0300

    simulador interactivo. calculo del costo de una clase con descuento e impuestos

--- index.js
+++ index.js
@@ -12,4 +12,16 @@ for (let index = 2; index <=4; index++){
     let result = numero * index;
     let mensaje = `${numero} x ${index} =${result}`;
     alert(mensaje); 
-}alert("Muchas gracias")
+}alert("Muchas gracias");
+
+const suma = (a,b) => a + b;
+const resta = (a,b) => a - b;
+const iva = x => x * 0.21
+
+let precioClase = 1700;
+let descuento = 850;
+
+let precioConDescuento = resta (suma(precioClase, iva(precioClase)), descuento)
+console.log(precioConDescuento)
+
+alert(`Precio final de la primera clase (50%off + IVA): ${precioConDescuento}ARS`)
