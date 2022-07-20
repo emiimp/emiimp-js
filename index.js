7a4dce8ec85eb87cbb7c7d1d5c4ecacd7eccf258
emiimp <emiliadasmode@gmail.com>
2022-07-19 22:10:28 -0300

    agregado de html,css,y mas funciones de js

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
+//DOM - SALUDO
+
 let nombre = prompt("Ingrese su nombre");
+let saludo = document.getElementById("saludo");
+console.log(saludo.innerHTML);
+saludo.innerHTML = `<h3>¡Bienvenid@, ${nombre}!</h3>`
 
-for(let i = 0; i <=3; i++){
-    {
-    alert(`¡4 veces hola, ${nombre}!`);
+//OPERACION
+
+const multiplicacion = (a,b) => a * b;
+
+let pesoArgentino = 1;
+let precioDolarOficial = 135.75;
+
+let cotizacionPeso = multiplicacion (pesoArgentino, precioDolarOficial)
+console.log(cotizacionPeso)
+
+alert(`Los precios están en USD. Para comprar en ARS, cotizamos según el dólar oficial del Banco Nación (Argentina): 1USD = ${cotizacionPeso}ARS`)
+
+//ARRAY + OBJECT
+
+const libros = [
+    {id: 1,titulo:"Moby Dick",autor:"Herman Melville",precio:12.86,vendido:false},
+    {id: 2,titulo:"The Picture of Dorian Gray",autor:"Oscar Wilde",precio:11.50,vendido:false},
+    {id: 3,titulo:"Dracula",autor:"Bram Stoker",precio:13,vendido:false},
+    {id: 4,titulo:"Frankenstein; or, The Modern Prometheus",autor:"Mary Shelley",precio:9.99,vendido:false},
+    {id: 5,titulo:"Grimm's Fairytales",autor:"Grimm Brothers",precio:7.56,vendido:false},
+    {id: 6,titulo:"Pride and Prejudice",autor:"Jane Austen",precio:9.99,vendido:false}
+
+];
+
+for (let index = 0; index < libros.length; index ++) {
+    console.log (libros[index]);
 }
+
+//HIGH ORDER FUNCTIONS
+
+let titulo = "";
+
+let ofertas = libros.filter(item => item.precio < 10);
+for(const item of ofertas){
+    titulo = titulo + item.titulo;
 }
 
-let numero = parseInt(prompt(`${nombre}, por favor ingresá un número para multiplicar tres veces`));
+alert(`Libros en oferta (precio menor a 10USD): ${titulo}. ¡No lo dejes pasar, ${nombre}!`)
 
-for (let index = 2; index <=4; index++){
-    let result = numero * index;
-    let mensaje = `${numero} x ${index} =${result}`;
-    alert(mensaje); 
-}alert("Muchas gracias");
 
-const suma = (a,b) => a + b;
-const resta = (a,b) => a - b;
-const iva = x => x * 0.21
 
-let precioClase = 1700;
-let descuento = 850;
-
-let precioConDescuento = resta (suma(precioClase, iva(precioClase)), descuento)
-console.log(precioConDescuento)
-
-alert(`Precio final de la primera clase (50%off + IVA): ${precioConDescuento}ARS`)
